ef4922b9d4d160ba912127ce62ffcfb622158606
peros <g00422221@atu.ie>
2025-12-11 20:16:17 +0000

    added helper function

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI, HTTPException
 from datetime import datetime
 from bson import ObjectId
-
+import os
+import aio_pika
+import json
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+from .models import notification_helper
 from .schemas import GetNotificationsWithoutState, Notification, NotificationUpdate
-from .models import entry_helper, project_helper
 from .configurations import db, notifications_collection
 app = FastAPI(title="Notifications API")
 
 currentUser = "691c8bf8d691e46d00068bf3"
+
+RABBIT_URL = os.getenv("RABBIT_URL")
+EXCHANGE_NAME = "notificiations_topic"
+if not RABBIT_URL:
+    raise RuntimeError("RABBIT_URL is not set. Export it (e.g. export RABBIT_URL='amqps://user:pass@host/vhost') or load your .env.")
+
+
 
 #******************************Notification endpoints**********************************
 #get all notifications
@@ -16,7 +28,8 @@ def get_notifications():
     notifications = list(notifications_collection.find({"user_id": currentUser}))
     if not notifications:
         raise HTTPException(status_code=404, detail="No notifications found")
-    return notifications
+    return [notification_helper(n) for n in notifications]
+
 
 # Get unread notifications
 @app.get("/notifications/unread", response_model=list[GetNotificationsWithoutState], status_code=200)
@@ -66,5 +79,39 @@ def delete_notifications():
 def delete_notifications_by_related_Id(related_id: str):
     notifications_collection.delete_many({"related_id": related_id})
     return {"message": "Notifications deleted"}
+
+##************************Rabbitmq messaging********************************
+
+
+
+async def consume_notifications():
+    connection = await aio_pika.connect_robust(RABBIT_URL)
+    channel = await connection.channel()
+    
+    exchange = await channel.declare_exchange("notificiations_topic", aio_pika.ExchangeType.TOPIC)
+    queue = await channel.declare_queue("entry_notifications", durable=True)
+    await queue.bind(exchange, routing_key="entry.completed")
+    
+    print("waiting for messages on entry_notifications ...")
+    
+    async with queue.iterator() as q:
+        async for message in q:
+            async with message.process():
+                body_bytes = bytes(message.body)
+                data = json.loads(body_bytes.decode("utf-8"))
+                
+                notification = {
+                    "user_id": data["user_id"],
+                    "message": f"Entry '{data['data']['name']}' completed",
+                    "related_id": data["data"]["id"],
+                    "opened": False,
+                    "created_at": datetime.now()
+                }
+                notifications_collection.insert_one(notification)
+
+@app.on_event("startup")
+async def startup_event():
+    import asyncio
+    asyncio.create_task(consume_notifications())
 
 # python -m uvicorn app.main:app --reload
--- app/models.py
+++ app/models.py
@@ -1,20 +1,7 @@
 
-# helper function to serialize MongoDB documents
-def entry_helper(entry) -> dict:
+def notification_helper(notification) -> dict:
     return {
-        "id": str(entry["_id"]),
-        "project_group_id": str(entry["project_group_id"]),
-        "name": entry["name"],
-        "starttime": entry["starttime"],
-        "endtime": entry.get("endtime"),
-        "duration": entry.get("duration"),
-    }
-
-#*********************Project managment models*************************
-def project_helper(project) -> dict:
-    return {
-        "id": str(project["_id"]),
-        "owner_id": str(project["owner_id"]),
-        "name": project["name"],
-        "description": project["description"]
+        "id": str(notification["_id"]),
+        "related_id": notification["related_id"],
+        "timestamp": notification["created_at"]
     }
